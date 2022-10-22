3f9ebf0cefba0096833625f5a6cef724d296c242
CrocodileGenna <yuramus1998@gmail.com>
2022-10-22 12:50:23 +0300

    FormData( )

--- js/task-08.js
+++ js/task-08.js
@@ -1,13 +1,21 @@
 const form = document.querySelector('.login-form');
 console.log(form);
-form.addEventListener('submit', (element)=>{
-    element.preventDefault();
-    const { elements: { 
-        email, 
-        password
-    } } = element.currentTarget;
-    email.value === '' || password.value === ''?
-    alert('Bсі поля повинні бути заповнені'):
-    console.log(`email: ${email.value}; passvord: ${password.value}`);
-    form.reset()
+// form.addEventListener('submit', (element)=>{
+//     element.preventDefault();
+//     const { elements: { 
+//         email, 
+//         password
+//     } } = element.currentTarget;
+//     email.value === '' || password.value === ''?
+//     alert('Bсі поля повинні бути заповнені'):
+//     console.log(`email: ${email.value}; passvord: ${password.value}`);
+//     form.reset()
+// });
+form.addEventListener('submit', (values)=>{
+    values.preventDefault();
+    const formData = new FormData(values.currentTarget);
+    formData.forEach((email, password)=>{
+        console.log('email: ', email);
+        console.log('password: ', password)
+    })
 })
